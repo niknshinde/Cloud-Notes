c3264cfd34bfc325277cfc49605ddb7e4cb13758
niknshinde <ns8830603789@gmail.com>
2024-01-03 10:02:06 +0530

    start using hash url for better

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-// import logo from './logo.svg';
-import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { NoteState } from './context/notes/NoteState';
 import Navbar from './components/Navbar';
@@ -14,48 +12,41 @@ import Alert from './components/Alert';
 import { useState } from 'react';
 import NoteDetail from './components/NoteDetail';
 
-
-
 function App() {
-
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type)=>{
-      setAlert({
-        msg: message,
-        type: type
-      })
-      setTimeout(() => {
-          setAlert(null);
-      }, 1500);
-  }
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
   return (
     <>
-    <AlertSate>
-    <NoteState>
+      <AlertSate>
+        <NoteState>
+          <Router>
+            <Navbar />
+            <Alert alert={alert} />
 
-      <Router>
-        <Navbar />
-        <Alert alert={alert}/>
-
-        <div className='container'>
-        <Routes>
-          <Route path="/" element={<Home1 showAlert={showAlert} />} />
-          <Route path="/about" element={<About1 />} />
-          <Route path="/login" element={<Login showAlert={showAlert}/>} />
-          <Route path="/signup" element={<SignUP showAlert={showAlert}/>} />
-          <Route path="/blogs/:id" element={<NoteDetail />} />
-
-
-        </Routes>
-        </div>
-      </Router>
-      </NoteState>
+            <div className='container'>
+              <Routes>
+                <Route path="/" element={<Home1 showAlert={showAlert} />} />
+                <Route path="/about" element={<About1 />} />
+                <Route path="/login" element={<Login showAlert={showAlert} />} />
+                <Route path="/signup" element={<SignUP showAlert={showAlert} />} />
+                <Route path="/blogs/:id" element={<NoteDetail />} />
+              </Routes>
+            </div>
+          </Router>
+        </NoteState>
       </AlertSate>
-
     </>
-    );
-  
+  );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ function Navbar() {
           <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} aria-current="page" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <Link className={`nav-link ${location.pathname === "/about" ? "active" : ""}`} to="/about">About Us</Link>
+          <Link className={`nav-link ${location.pathname === "/#about" ? "active" : ""}`} to="/about">About Us</Link>
         </li>
         </ul>
       {!localStorage.getItem('token') ? <form className="d-flex" role="search">
